066d54ce125f72d20442260b36424ced36def3b7
akrambj <bedjaouiakram.93@gmail.com>
2024-07-16 16:39:40 +0100

    services

--- src/pages/SharedLayout.jsx
+++ src/pages/SharedLayout.jsx
@@ -4,10 +4,10 @@ import Footer from "../components/shared/Footer";
 
 const SharedLayout = () => {
   return (
-    <main className="w-full  flex  flex-col">
-      <div>
-        <Header />
-      </div>
+    <main className="w-full  flex  flex-col overflow-x-hidden">
+        <div>
+          <Header />
+        </div>
       <div>
         <Outlet />
       </div>
